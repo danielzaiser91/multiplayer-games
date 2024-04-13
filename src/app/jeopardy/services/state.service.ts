dcbee70ef82b8f4f71c0ba3723afec5eb91b0d8b
Daniel Zaiser <danielzaiser91@googlemail.com>
2024-04-13 17:50:56 +0200

    remove ssr and persist state

--- src/app/jeopardy/services/state.service.ts
+++ src/app/jeopardy/services/state.service.ts
@@ -1,29 +1,31 @@
-import { Injectable } from "@angular/core";
+import { Inject, Injectable, afterRender } from "@angular/core";
 import { Store } from "@ngrx/store";
 import { JeopardyState, initialState, jeopardyFeature } from "../store$/feature";
 import { overwriteState } from "../store$/actions";
+import { DOCUMENT } from "@angular/common";
 
 const JEOPARDY_STATE_KEY = 'state:jeopardy';
 
 @Injectable()
 export class StateService {
   jeopardyState = initialState;
+
   constructor(private store: Store) {
-    this.store.select<JeopardyState>(jeopardyFeature.selectJeopardyState).subscribe(state => this.jeopardyState = state);
-    setInterval(() => {
+    addEventListener('beforeunload', () => {
       this.saveState();
-    }, 1000);
+    });
     this.loadState();
+    this.store.select<JeopardyState>(jeopardyFeature.selectJeopardyState).subscribe(state => this.jeopardyState = state);
   }
 
   private saveState() {
-    localStorage?.setItem(JEOPARDY_STATE_KEY, JSON.stringify(this.jeopardyState));
+    localStorage.setItem(JEOPARDY_STATE_KEY, JSON.stringify(this.jeopardyState));
   }
   
   private loadState() {
     try {
-      const state = JSON.parse(localStorage.getItem(JEOPARDY_STATE_KEY)!);
-      const newState: JeopardyState = Object.assign(initialState, (state));
+      const state = JSON.parse(localStorage.getItem(JEOPARDY_STATE_KEY) as string);
+      const newState: JeopardyState = Object.assign({}, initialState, state);
       this.store.dispatch(overwriteState({ newState }));
     } catch (e) {
       console.error(e);
