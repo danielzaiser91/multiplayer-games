137d2f4d88ee32a7bc6bd0b68090e2cb8787f50b
Niko <niko.kirmes@dacor.de>
2024-04-21 14:59:13 +0200

    setup socket service

--- src/app/jeopardy/login/login.component.ts
+++ src/app/jeopardy/login/login.component.ts
@@ -3,6 +3,7 @@ import { Store } from '@ngrx/store';
 import { search } from '../store$/actions';
 import { jeopardyFeature } from '../store$/feature';
 import { StateService } from '../services/state.service';
+import { SocketService } from '../services/socket.service';
 
 @Component({
   selector: 'app-login',
@@ -12,9 +13,20 @@ import { StateService } from '../services/state.service';
 export class LoginComponent {
   query$ = this.store.select(jeopardyFeature.selectJeopardyState)
 
-  constructor(private store: Store, private stateService: StateService) {}
+  passphrase: number = 0;
 
-  onTestClick(query: string) {
+  constructor(private store: Store, private stateService: StateService, private socketService: SocketService) {}
+
+  public onTestClick(query: string) {
     this.store.dispatch(search({ query }));
+  }
+
+  public onClickCreateQuiz(){
+    this.passphrase = this.socketService.createQuiz();
+    console.log(this.passphrase)
+  }
+
+  public onClickJoinQuiz(pwd: string){
+    this.socketService.joinQuiz(parseInt(pwd))
   }
 }
